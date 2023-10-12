62840870da230de82eee28da37488f4dcc7d25f6
daniel <sanz.daniel95@gmail.com>
2023-10-12 12:00:44 +0200

    adding lecture II project

---
+++ Cours02/Program.cs
@@ -0,0 +1,6 @@
+﻿using Cours02;
+Calculator calc = new Calculator();
+
+Console.WriteLine(calc.Add(2, 3));      // Affiche 5 (en utilisant la surcharge #1)
+Console.WriteLine(calc.Add(2, 3, 4));   // Affiche 9 (en utilisant la surcharge #2)
+Console.WriteLine(calc.Add(2.5, 3.5));  // Affiche 6.0 (en utilisant la surcharge #3)
